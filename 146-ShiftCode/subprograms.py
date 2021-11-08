050e5206559ad91994fed91da1615d044d1d0e22
[symonkipkemei] <[symonkipkemei@gmail.com]>
2021-11-08 23:45:45 +0300

    initial commit

---
+++ 146-ShiftCode/subprograms.py
@@ -0,0 +1,18 @@
+# there are two parameters needed for this programme.
+# We are going to create a sub-programme that picks the following items
+
+
+# MESSAGE
+def userMessage():
+    """Pick the message for the programme"""
+    message = input("enter message : ")
+
+    return message
+
+
+# NUMBER
+def userNumber():
+    """Picks the number of times the code should be shifted by"""
+    number = input("enter number : ")
+
+    return number
