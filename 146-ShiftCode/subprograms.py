da7783df4b419d1850655f0a802a66cc3d93ba70
[symonkipkemei] <[symonkipkemei@gmail.com]>
2021-11-09 00:34:40 +0300

    the shift code update

--- 146-ShiftCode/subprograms.py
+++ 146-ShiftCode/subprograms.py
@@ -13,6 +13,45 @@ def userMessage():
 # NUMBER
 def userNumber():
     """Picks the number of times the code should be shifted by"""
-    number = input("enter number : ")
+    number = int(input("enter number : "))
 
     return number
+
+
+# THE ENCODER
+
+def encoder(message, number):
+    listMessage = list(message)
+    newMessage = []
+    editedMessage = []
+
+    alphabets = "abcdefghijklmnopqrstuvwxyz"
+    alphabets = list(alphabets)
+    for y in listMessage:
+        for index,x in enumerate(alphabets):
+            if y == x:
+                point = index
+                newMessage.append(point)
+    print(newMessage)
+
+
+    #decoding by the number
+
+    for z in newMessage:
+        editedNumber = int(z)+number
+        editedMessage.append(editedNumber)
+
+    print(editedMessage)
+
+
+
+# DECODING
+
+
+def main():
+    message= userMessage()
+    number = userNumber()
+
+    encoder(message, number)
+
+main()
