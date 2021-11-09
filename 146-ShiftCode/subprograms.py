d36c6b556c79a6b687036dfbc60471bc89c671eb
[symonkipkemei] <[symonkipkemei@gmail.com]>
2021-11-09 23:43:35 +0300

    the encoder

--- 146-ShiftCode/subprograms.py
+++ 146-ShiftCode/subprograms.py
@@ -28,30 +28,79 @@ def encoder(message, number):
     alphabets = "abcdefghijklmnopqrstuvwxyz"
     alphabets = list(alphabets)
     for y in listMessage:
-        for index,x in enumerate(alphabets):
+        for index, x in enumerate(alphabets):
             if y == x:
                 point = index
                 newMessage.append(point)
     print(newMessage)
 
-
-    #decoding by the number
+    # decoding by the number
 
     for z in newMessage:
-        editedNumber = int(z)+number
+        editedNumber = int(z) + number
         editedMessage.append(editedNumber)
 
-    print(editedMessage)
+    # displaying the decoded message
 
+    decodedMessage = []
+
+    for x in editedMessage:
+        for indexing, y in enumerate(alphabets):
+            if indexing == x:
+                decodedMessage.append(y)
+
+    ujumbe = ''
+
+    for z in decodedMessage:
+        ujumbe += z
+
+    tuple = (ujumbe, alphabets)
+
+    return tuple
 
 
 # DECODING
 
 
+def decoder(encoded, alphabets, number):
+    """ We are going to decode the message we just coded above"""
+
+    # convert message to index
+    msg = []
+    for y in encoded:
+        for index, x in enumerate(alphabets):
+            if y == x:
+                msg.append(index)
+
+    newMsg = []
+
+    for z in msg:
+        newNo = z - number
+        newMsg.append(newNo)
+
+    # change the decoded msg to alphabets
+    newMsgWord = []
+
+    for l in newMsg:
+        for indexs, x in enumerate(newMsg):
+            if l == indexs:
+                newMsgWord.append(x)
+
+    msg1 = ""
+
+    for x in newMsgWord:
+        msg1 += x
+    print(msg1)
+
+
+
 def main():
-    message= userMessage()
+    message = userMessage()
     number = userNumber()
 
-    encoder(message, number)
+    ujumbe, alphabets = encoder(message, number)
+
+    decoder(ujumbe, alphabets, number)
+
 
 main()
